86b8b8746d73e2c323a45a60d972f7df764e51e4
Aaron Moore <aaron@atamisk.net>
2022-06-01 22:44:36 -0400

    Added a structure to simultaneously represent relu and its derivative.

--- src/activations.rs
+++ src/activations.rs
@@ -1,5 +1,33 @@
 use std::f64::consts::E as e;
 
+trait Activation {
+    fn act(x: f64) -> f64;
+    fn act_prime(x: f64) -> f64;
+}
+
+struct Relu(());
+
+impl Activation for Relu{
+    fn act(x: f64) -> f64 {
+        ensure_finite(x);
+        if x < 0.0 {
+            0.0
+        }
+        else {
+            x
+        }
+    }
+
+    fn act_prime(x: f64) -> f64 {
+        ensure_finite(x);
+        if x <= 0.0 {
+            0.0
+        } else {
+            1.0
+        }
+    }
+}
+
 fn ensure_finite(x: f64) -> () {
     if !x.is_finite(){
         panic!("Activation is not finite!");
